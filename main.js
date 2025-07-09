ce70977ff58a2d5abad2c3787d8265969d059d6a
xJuStInJuStIcEx <xx.justinjustice.xx@gmail.com>
2025-07-09 18:29:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ function update(dt) {
   }
 
   // Spawn
-  if (Math.random() 0.8 * dt) spawnElement();
+  if (Math.random() < 2 * dt) spawnElement();
 
   // Muovi e filtra elementi
   elements = elements.filter(el => {
